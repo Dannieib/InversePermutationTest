04acc8816b9fe758a17d022895a40ef579c7cd59
Daniel Ibanga <danielibanga4321@outlook.com>
2021-09-13 08:50:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace InversePermutationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 4, 5, 1 };
+            var resp = InversePermutation(arr, 5);
+            var output = String.Join(" ", resp);
+
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+
+        public static int[] InversePermutation(int[] arrNum, int output)
+        {
+            int[] brr = new int[output];
+
+            for (int i = 0; i < output; i++)
+            {
+                int pridx = arrNum[i] - 1;
+                brr[pridx] = i + 1;
+            }
+
+            for (int i = 0; i < output; i++)
+            {
+                if (arrNum[i] != brr[i])
+                {
+                    return brr;
+                }
+            }
+            return brr;
+        }
+    }
+}
